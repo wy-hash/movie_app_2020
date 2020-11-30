801ebd30c8e0d1e0032ab026e0e97ad7791c58bc
wy-hash <wy.hash@gmail.com>
2020-11-30 18:27:38 +0900

    영화 웹 사이트 만들기 - 영화목록 api 주소

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ class App extends React.Component {
   constructor(props) {
     super(props);
   }
-  state = { isLoading: true };
+  state = { isLoading: true, movies: [] };
 
   componentDidMount() {
+    // 영화 데이터 로딩
     setTimeout(() => {
       this.setState({ isLoading: false });
     }, 6 * 1000);
@@ -16,8 +17,11 @@ class App extends React.Component {
 
   render() {
     const { isLoading } = this.state;
-    return <div>{isLoading ? "Loding..." : "We are ready"}</div>;
+    return <div>{isLoading ? "Loding..." : "We are ready"}</div>; // "We are ready" : 영화데이터가 추후 들어갈 곳
   }
 }
 
 export default App;
+
+//https://yts.mx/api/v2/list_movies.json
+// 노마드 코더의 영화목록 API 주소 변경 x -> https://yts-proxy.now.sh/list_movies.json
