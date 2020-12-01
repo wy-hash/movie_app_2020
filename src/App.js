3df1aba11b8e08aa36fdb591436781734a07eb67
wy-hash <wy.hash@gmail.com>
2020-12-01 10:42:22 +0900

    영화 웹 사이트 만들기 - 영화목록 api 호출 완료

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   constructor(props) {
@@ -6,18 +8,47 @@ class App extends React.Component {
   }
   state = { isLoading: true, movies: [] };
 
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    console.log(movies);
+    //const movies
+    //console.log(movies.data.data.movies);
+    this.setState({ movies, isLoading: false });
+  };
+
   componentDidMount() {
     // 영화 데이터 로딩
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6 * 1000);
+    this.getMovies();
   }
   componentDidUpdate() {}
   componentWillUnmount() {}
 
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loding..." : "We are ready"}</div>; // "We are ready" : 영화데이터가 추후 들어갈 곳
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loding..."
+          : movies.map((movie) => {
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}
+      </div>
+    ); // "We are ready" : 영화데이터가 추후 들어갈 곳
   }
 }
 
@@ -25,4 +56,6 @@ export default App;
 
 //https://yts.mx/api/v2/list_movies.json
 // 노마드 코더의 영화목록 API 주소 변경 x -> https://yts-proxy.now.sh/list_movies.json
-// 노마드 코더의 영화상세 API 주소 변경 x -> https://yts-proxy.now.sh/movie-details.json
+
+// 노마드 코더의 영화상세 API 주소 변경 x -> https://yts-proxy.now.sh/movie_details.json?movie_id=24286
+// movie_id		Integer (Unsigned)	null	The ID of the movie+
