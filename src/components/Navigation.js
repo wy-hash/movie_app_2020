db9d3366cd9ce45cd35e39dc761d158332b47ec1
wy-hash <wy.hash@gmail.com>
2020-12-01 18:14:38 +0900

    Detail 컴포넌트 만들기 전까지 233p

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,6 +6,7 @@ function Navigation() {
   return (
     <div className="nav">
       <Link to="/">Home</Link>
+      {/* <Link to={{ pathname: "/about", state: { fromNavigation: true } }}></Link> */}
       <Link to="/about">About</Link>
     </div>
   );
